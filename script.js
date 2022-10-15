c360f9ae46e14b912be762b8ec3fc5bdd4b2d8b8
Laszlo Rozsnyai <CV-Laszlo-91@tiscali.it>
2022-10-15 16:00:35 +0200

    aggiunta esperenza

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times');
         $('header').toggleClass('toggle');
     });
-    $('#icon-x').click(function(){
-        $(this).toggleClass('icon_rotate');
-        $('.works .container').toggleClass('workon');
+    $('.works #icon-x').click(function() {
+        $(this).removeClass('on');
+        $('.works .accordion-box').removeClass('on');
     });
     $('#palette').click(function() {
         $(this).toggleClass('icon-rotate');
@@ -19,7 +19,11 @@ $(document).ready(function(){
     //funzione per far scomparire le carte skill 
     $('#click-skills, #click-soft, #click-tool').click(function() {
         $('.skills #icon-x-skill').toggleClass('on');
-        $('.skills .skill-container').toggleClass('off');
+        $('.skills .skill-container').toggleClass('animate__animated animate__backOutUp');
+        window.setTimeout(function(){
+            $('.skills .skill-container').toggleClass('off');
+        },750);
+        //$('.skills .skill-container').toggleClass('off');
     });
     $('#click-skills').click(function() {
         $('.skill-box').toggleClass('on');
@@ -34,8 +38,8 @@ $(document).ready(function(){
         $('.skill-box.on').removeClass('on');
         $('.soft-box.on').removeClass('on');
         $('.tools-box').removeClass('on');
+        $('.skills .skill-container.animate__animated.animate__backOutUp').removeClass('animate__animated animate__backOutUp');
     });
-   
     // fare funzione apparire skills
     //fare funzione apparire soft 
     //fare funzione apparire tools
@@ -44,7 +48,26 @@ $(document).ready(function(){
         $('.skills #icon-x-skill').removeClass('on');
         $('.skills #skill-container').removeClass('off');
     });
-
+    $('.works .book-container .book-card .details #extra-work').click(function(){
+        $('.book-title').toggleClass('off');
+        $('.book-container').toggleClass('animate__animated animate__backOutUp');
+        window.setTimeout(function(){
+            $('.book-container').toggleClass('off');
+        },750);
+        $('.works #icon-x').toggleClass('on');
+        $('.works .accordion-box-1').toggleClass('on');
+    });
+    $('.works #icon-x').click(function() {
+        $('.works #icon-x.on').removeClass('on');
+        $('.works .accordion-box-1.on').removeClass('on');
+        $('.book-title.off').removeClass('off');
+        $('.book-container.off').removeClass('off');
+        $('.book-container.animate__animated.animate__backOutUp').removeClass('animate__animated animate__backOutUp');
+        $('.book-container').toggleClass('animate__animated animate__backInDown');
+        window.setTimeout(function(){
+            $('.book-container.animate__animated.animate__backInDown').removeClass('animate__animated animate__backInDown');
+        },750);
+    });
     $(window).on('scroll load',function(){
         $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
@@ -76,4 +99,11 @@ themColor.forEach(color => color.addEventListener('click', () => {
     //document.querySelector(':root').style.background = background;
     document.documentElement.style.setProperty('--yellow',background);
 }));
+
+const accordion = document.getElementsByClassName('contentbox');
+for(i=0 ; i<accordion.length; i++) {
+    accordion[i].addEventListener('click', function(){
+        this.classList.toggle('active')
+    });
+}
 
